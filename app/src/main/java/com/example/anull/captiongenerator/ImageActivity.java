5370972163e5fe1e4b2a59ae99e729359b451e8d
rajan.garg <iamrajangarg@gmail.com>
2017-01-11 21:30:08 +0530

    added cognitive-vision../ sample application which analyze the image separately

--- app/src/main/java/com/example/anull/captiongenerator/ImageActivity.java
+++ app/src/main/java/com/example/anull/captiongenerator/ImageActivity.java
@@ -4,11 +4,10 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.Button;
 
-/**
- * Created by AbhinavP on 1/9/2017.
- */
 public class ImageActivity extends AppCompatActivity{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -16,5 +15,16 @@ public class ImageActivity extends AppCompatActivity{
         setContentView(R.layout.activity_image);
         ImageView imageView = (ImageView) findViewById(R.id.imageId);
         imageView.setImageBitmap(MainActivity.galleryImageBitmap);
+
+        Button nextButton = (Button) findViewById(R.id.next_btn);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ;
+            }
+        });
+
     }
+
+
 }
